9123d8f5d270d648caca6f7328dd3236a4c4ec3b
Fhiz <support@wovencode.net>
2020-07-03 18:04:23 +0200

    Update MailManagerConfig.class.php

--- Data/Config/ManagerConfig/MailManagerConfig.class.php
+++ Data/Config/ManagerConfig/MailManagerConfig.class.php
@@ -13,12 +13,12 @@ use phramework;
 class MailManagerConfig extends BaseConfig
 {
 		
-	public const CONST_MAIL_EMAIL 				= 'test@wovencode.net';
-	public const CONST_MAIL_ECONST_MAIL_REPLY	= 'support@wovencode.net';
-	public const CONST_MAIL_ECONST_MAIL_FROM	= 'support@wovencode.net';
-	public const CONST_MAIL_HOST				= 'imap.1und1.de';
+	public const CONST_MAIL_EMAIL 				= '';
+	public const CONST_MAIL_ECONST_MAIL_REPLY	= '';
+	public const CONST_MAIL_ECONST_MAIL_FROM	= '';
+	public const CONST_MAIL_HOST				= '';
 	public const CONST_MAIL_PORT				= '993';
-	public const CONST_MAIL_PASSWORD			= 'BaBw5lCAEKVVzPvvW1H!';
+	public const CONST_MAIL_PASSWORD			= '';
 	
 	/*
 	* Do not change anything below unless you know what you are doing!
